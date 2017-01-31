3c267a7b209308da0384ab000a5cebf1ee4f24d6
Matthew Ebert <mfe5003@gmail.com>
2017-01-31 13:48:48 -0600

    cleaned up project

---
+++ pdflatex_chonly.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#pdflatex -jobname="ryd_udd" "\includeonly{app_blockadeshifts/app_blockadeshifts}\input{main.tex}"
+pdflatex -jobname="modeling" "\includeonly{app_modeling/app_modeling}\input{main.tex}"
